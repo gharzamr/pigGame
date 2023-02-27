b4f131a81e079bd9216e373e9a1bf92a7ba2c4f0
gharzamr <amr@Ontimes-MacBook-Air.local>
2023-02-27 21:50:52 +0400

    Switch the player

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // Selecting elements
-
+let player0El = document.querySelector('.player--0');
+let player1El = document.querySelector('.player--1');
 let score0El = document.querySelector('#score--0');
 let score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
@@ -17,6 +18,7 @@ score1El.textContent = '0';
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 
@@ -32,8 +34,15 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore = currentScore + dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
